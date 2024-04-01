4e6f0f7d80f7c615c3e1d482988afc8d74cdd045
yuangwei <yuang.wya@gmail.com>
2024-04-01 16:04:19 +0800

    Initial Commit

---
+++ astro.config.mjs
@@ -0,0 +1,13 @@
+import { defineConfig } from 'astro/config';
+import mdx from "@astrojs/mdx";
+import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    tailwind(),
+    sitemap(),
+    mdx(),
+  ],
+});
