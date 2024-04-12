8b335af1029c61de8d0c2739abbbfcce909f98e7
yuangwei <yuang.wya@gmail.com>
2024-04-12 16:00:26 +0800

    Initial Commit

---
+++ app/robots.ts
@@ -0,0 +1,11 @@
+export default function robots() {
+  return {
+    rules: [
+      {
+        userAgent: '*',
+      },
+    ],
+    sitemap: 'https://yuangwei.com/sitemap.xml',
+    host: 'https://yuangwei.com',
+  };
+}
