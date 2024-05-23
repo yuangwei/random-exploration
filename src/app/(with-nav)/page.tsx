82837f125773347fcb4b4cae5b3d66b995bbf505
yuangwei <yuang.wya@gmail.com>
2024-05-23 15:28:05 +0800

    Simple home page

--- src/app/(with-nav)/page.tsx
+++ src/app/(with-nav)/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
       <h1 className="font-bold">Hi, I am Yuang Wei</h1>
       <ul className="not-italic flex gap-5 mt-4 text-sm font-medium">
         <Link href="/blog" className="hover:underline underline-offset-4">Blog</Link>
-        <Link href="https://x.com/_yuangwei" className="hover:underline underline-offset-4 ">Twitter</Link>
+        <Link href="https://x.com/yuangwya" className="hover:underline underline-offset-4 ">Twitter</Link>
         <Link href="https://linkedin.com/in/yuangwei" className="hover:underline underline-offset-4 ">Linkedin</Link>
       </ul>
     </main>
